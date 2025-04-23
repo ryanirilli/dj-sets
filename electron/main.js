394e57a967073eb6732b2fc2f80f8010a07ae4d1
Ryan Irilli <ryanirilli@gmail.com>
2025-04-22 21:56:34 -0700

    fix prod electron build

--- electron/main.js
+++ electron/main.js
@@ -1,6 +1,7 @@
 const { app, BrowserWindow } = require("electron");
 const path = require("path");
-const isDev = process.env.NODE_ENV !== "production";
+// Use Electron's built-in method to check if running packaged
+const isDev = !app.isPackaged;
 
 function createWindow() {
   const mainWindow = new BrowserWindow({
